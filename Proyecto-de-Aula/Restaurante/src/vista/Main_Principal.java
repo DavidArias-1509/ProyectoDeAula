ee127b23638aa5f8b7b229e5586d512d3ceac8c8
DavidArias-1509 <davidariass0519@gmail.com>
2023-06-10 20:40:26 -0500

    update

--- Proyecto-de-Aula/Restaurante/src/vista/Main_Principal.java
+++ Proyecto-de-Aula/Restaurante/src/vista/Main_Principal.java
@@ -1,12 +1,9 @@
 package vista;
 
-<<<<<<< Updated upstream
 /*
  * @author david
  */
 
-=======
->>>>>>> Stashed changes
 public class Main_Principal {
     
     public static void main(String[] args) {
